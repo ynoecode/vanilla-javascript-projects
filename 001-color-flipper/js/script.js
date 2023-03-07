2e17aada35389cc97aa73de061b5dbb98b0e9408
Francis Ignacio <ynoecode@gmail.com>
2023-03-07 23:17:41 +0800

    perf: make some changes & optimization in js
    Also added some comments per line or algorithm.

--- 001-color-flipper/js/script.js
+++ 001-color-flipper/js/script.js
@@ -1,38 +1,53 @@
 // Get the elements to be updated
 const bgTitleEl = document.querySelector('.bg-title');
 const bgColorEl = document.querySelector('.bg-color');
+const buttonEl = document.querySelector('#btn');
 
-// Add an event listener to the button
-document.querySelector('#btn').addEventListener('click', () => {
-  // Generate a random hex color code for the background
+// Add an event listener to the button that calls updateBackgroundColor when clicked
+buttonEl.addEventListener('click', updateBackgroundColor);
+
+// Updates the background color and text when the button is clicked
+function updateBackgroundColor() {
+  // Generate a random hex color code
   const hexColor = getRandomHexColor();
-  // Set the background color of the content container
+  // Determine whether the background color is dark or light
+  const isDark = isDarkColor(hexColor);
+
+  // Set the background color of the page
   document.body.style.backgroundColor = hexColor;
-  // Set the text color of the content container based on the background color
-  document.body.style.color = isDarkColor(hexColor) ? 'white' : 'black';
+  // Set the text color of the page based on the background color
+  document.body.style.color = isDark ? 'white' : 'black';
   // Update the background color title and text
   bgTitleEl.textContent = `Background Color: ${hexColor}`;
   bgColorEl.textContent = hexColor;
-});
-
-// Helper function to generate a random hex color code
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// Helper function to determine if a hex color code is dark or light
+// Generates a random hex color code
+function getRandomHexColor() {
+  // Generate a random number between 0 and 16777215 (decimal equivalent of FFFFFF)
+  const randomColor = Math.floor(Math.random() * 16777215);
+  // Convert the decimal number to a hexadecimal string and prepend '#' character
+  return `#${randomColor.toString(16).padStart(6, '0')}`;
+}
+
+// Determines whether a hex color code is dark or light
 function isDarkColor(hexColor) {
-  const rgb = hexToRgb(hexColor);
-  const brightness = ((rgb.r * 299) + (rgb.g * 587) + (rgb.b * 114)) / 1000;
+  // Convert the hex color code to an RGB object
+  const { r, g, b } = hexToRgb(hexColor);
+  // Calculate the brightness of the color using the formula for relative luminance
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+  // Return true if the brightness is less than 128, indicating a dark color
   return brightness < 128;
 }
 
-// Helper function to convert a hex color code to an RGB object
+// Converts a hex color code to an RGB object
 function hexToRgb(hexColor) {
+  // Remove the '#' character from the hex color code
   const hex = hexColor.replace('#', '');
-  return {
-    r: parseInt(hex.substring(0, 2), 16),
-    g: parseInt(hex.substring(2, 4), 16),
-    b: parseInt(hex.substring(4, 6), 16),
-  };
+  // Convert the red, green, and blue values from hexadecimal to decimal
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+  // Return an object with the red, green, and blue values
+  return { r, g, b };
 }
